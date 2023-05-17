002257abee8f2e375d2b7e3de862dbc5d1ffc3e6
Uchchash346 <mru416@gmail.com>
2023-05-17 18:00:49 +0600

    change some design and load data

--- app/src/main/java/com/uchchash/storyapp/NextActivity.java
+++ app/src/main/java/com/uchchash/storyapp/NextActivity.java
@@ -3,10 +3,12 @@ package com.uchchash.storyapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class NextActivity extends AppCompatActivity {
     TextView txt;
+    ImageView image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +16,6 @@ public class NextActivity extends AppCompatActivity {
         txt = findViewById(R.id.nxt);
         String t = getIntent().getStringExtra("key");
         txt.setText(t);
+
     }
 }
